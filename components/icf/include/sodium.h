bcba1ff5423a5a15009b3667eb04b13d6f62ab8d
Lionel ORCIL <48251459+ioio2995@users.noreply.github.com>
2025-07-29 19:25:44 +0200

    change openssl by mbedtls

--- components/icf/include/sodium.h
+++ components/icf/include/sodium.h
@@ -2,12 +2,20 @@
 #define SODIUM_H
 #include <stdint.h>
 #include <stddef.h>
-#include <openssl/sha.h>
+#include "mbedtls/sha256.h"
+
 #define crypto_hash_sha256_BYTES 32
+
 static inline int crypto_hash_sha256(uint8_t *out, const uint8_t *in, unsigned long long inlen) {
-    SHA256(in, inlen, out);
+    mbedtls_sha256_context ctx;
+    mbedtls_sha256_init(&ctx);
+    mbedtls_sha256_starts(&ctx, 0);
+    mbedtls_sha256_update(&ctx, in, inlen);
+    mbedtls_sha256_finish(&ctx, out);
+    mbedtls_sha256_free(&ctx);
     return 0;
 }
+
 static inline int crypto_sign_verify_detached(const unsigned char *sig, const unsigned char *m, unsigned long long mlen, const unsigned char *pk) {
     (void)sig; (void)m; (void)mlen; (void)pk;
     return -1; // not implemented, always fail
